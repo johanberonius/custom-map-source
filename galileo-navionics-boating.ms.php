314c83a086a001ab5e0bfd3f9b8c8ebb367ae746
Johan Beronius <johan.beronius@athega.se>
2016-07-01 12:20:40 +0200

    Fixed whitespace in php files.

--- galileo-navionics-boating.ms.php
+++ galileo-navionics-boating.ms.php
@@ -1,25 +1,24 @@
 <?php
-        $url = 'http://backend.navionics.io/tile/get_key/Navionics_internalpurpose_00001/webapp.navionics.com';
-        $referer = 'https://webapp.navionics.com';
-        $mapsource = '<?xml version="1.0" encoding="UTF-8"?>
+$url = 'http://backend.navionics.io/tile/get_key/Navionics_internalpurpose_00001/webapp.navionics.com';
+$referer = 'https://webapp.navionics.com';
+$mapsource = '<?xml version="1.0" encoding="UTF-8"?>
 <customMapSource>
 <name>Navionics Boating</name>
 <url>http://backend.navionics.io/tile/{$z}/{$x}/{$y}?LAYERS=config_1_6.00_0&TRANSPARENT=FALSE&UGC=TRUE&navtoken={$navtoken}</url>
 </customMapSource>';
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_REFERER, $referer);
-        $navtoken = curl_exec($ch);
-        $info = curl_getinfo($ch);
-        curl_close($ch);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_REFERER, $referer);
+$navtoken = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);
 
-        if ($info["http_code"] == 200) {
-            header('Content-type: application/x-galileo');
-            echo str_replace('{$navtoken}', urlencode($navtoken), $mapsource);
-        } else {
-            header('Content-type: text/plain');
-            echo 'Something went wrong';
-        }
-
+if ($info["http_code"] == 200) {
+    header('Content-type: application/x-galileo');
+    echo str_replace('{$navtoken}', urlencode($navtoken), $mapsource);
+} else {
+    header('Content-type: text/plain');
+    echo 'Something went wrong';
+}
